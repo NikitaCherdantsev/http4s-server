39c058ffdda4975f23b69dca2075b63778161116
nikita7777o <45508813+nikita7777o@users.noreply.github.com>
2020-03-26 02:01:19 +0500

    stream aded

--- src/main/scala/JsonService.scala
+++ src/main/scala/JsonService.scala
@@ -2,7 +2,9 @@ package project
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+
 import cats.effect._
+import fs2.Stream
 import io.circe._
 import io.circe.generic.semiauto.deriveEncoder
 import io.circe.syntax._
@@ -10,17 +12,34 @@ import org.http4s._
 import org.http4s.circe._
 import org.http4s.dsl.io._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
 object JsonService {
 
   implicit val jsonsEncoder: Encoder[JsonInfo] = deriveEncoder[JsonInfo]
+  implicit val timer: Timer[IO]                = IO.timer(global)
+  implicit val cs: ContextShift[IO]            = IO.contextShift(global)
 
   val jsonService: HttpRoutes[IO] =
     HttpRoutes.of[IO] {
       case GET -> Root / "json" / msg =>
+        val jsonStream = Stream
+          .repeatEval(IO(JsonInfo(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")), msg).asJson))
+          .metered(3.second)
+          .interruptAfter(10.seconds)
         for {
-          resp <- Ok(JsonInfo(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")), msg).asJson)
+          resp <- Ok(jsonStream)
         } yield resp
     }
+
+//  val jsonService: HttpRoutes[IO] =
+//    HttpRoutes.of[IO] {
+//      case GET -> Root / "json" / msg =>
+//        for {
+//          resp <- Ok(JsonInfo(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")), msg).asJson)
+//        } yield resp
+//    }
 
 }
 
